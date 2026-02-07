751a9d80d63ba258f701f8491bd90d72f4947247
Cem-Kaplan <cemk46908@gmail.com>
2026-02-07 07:52:32 +0100

    Strings und weitere operatoren gelernt in javascript

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -15,7 +15,7 @@ Label.textContent = textInhalt;
 //let nutzerName = window.prompt("Dein Name: ");
 //Label.textContent = nutzerName;
 
-// forms
+// forms mit if else
 let Button = document.getElementById("Send_Button_1")
 Button.onclick = function(){
     nutzerName2 = document.getElementById("Input_1").value;
@@ -25,3 +25,22 @@ Button.onclick = function(){
         console.log("Dein Name ist: ", nutzerName2);
     }
 }
+
+//ternärer Operator
+let Kosten = 10;
+let Rabatt = 2;
+let Stammkunde = true;
+
+let endPreis = Stammkunde ? (Kosten - Rabatt) : Kosten;
+console.log("Endpreis: ", endPreis);
+
+// for loops
+
+for (let i = 0; i < 5; i++) {
+    console.log("Aktuelle Zahl: ", i);
+}
+
+// template strings 
+
+let template_string = `Der textInhalt inhalt ist ${textInhalt}`;
+console.log("Template string: ", template_string);
